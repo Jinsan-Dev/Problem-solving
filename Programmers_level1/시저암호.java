1432952c224d3abc75fe6c7bf57308870910295f
Jinsan Kim <jinsan.dev@gmail.com>
2022-03-03 23:03:05 +0900

    Update and rename 시저암호.py to 시저암호.java

--- Programmers_level1/시저암호.java
+++ Programmers_level1/시저암호.java
@@ -1,3 +1,31 @@
+class Solution {
+    public String solution(String s, int n) {
+        char[] answer = s.toCharArray();
+        int temp = 0;
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i) == ' '){
+                continue;
+            }
+            temp = (int)s.charAt(i) + n;
+            if(64 < s.charAt(i) && s.charAt(i) < 91){ // uppercase
+                if(90 < temp){
+                    temp = temp-90+64;
+                }
+            }else{
+                if(122 < temp){
+                    temp = temp-122+96;
+                }
+            }
+            answer[i] = (char) temp;
+            temp = 0;
+        }
+        String str = new String(answer);
+        return str;
+    }
+}
+
+---------------------------------------------------------
+
 def solution(s, n):
     answer = list(s)
     new = []
