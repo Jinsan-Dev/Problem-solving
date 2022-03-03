1154e69bdd3ec6ba8467117749c3993267c46867
Jinsan Kim <jinsan.dev@gmail.com>
2022-03-03 23:35:00 +0900

    Update 두_정수_사이의_합.java

--- Programmers_level1/두_정수_사이의_합.java
+++ Programmers_level1/두_정수_사이의_합.java
@@ -1,48 +1,32 @@
 import java.util.*;
-import java.lang.Math;
 
 class Solution {
     public long solution(long a, long b) {
-        if(a == b){ return a; }
+        long answer = 0;
         
-        if((a>0 && b>0) || (a<0 && b<0)){
-            boolean flag = true;
-            long temp = 0;
-            if(a<0 && b<0) { flag = false; }
-        
-            long big = Math.max(Math.abs(a),Math.abs(b));
-            long small = Math.min(Math.abs(a),Math.abs(b));
-            
-            for(long i=small;i<big+1;i++){
-                temp+=(long)i;
-            }
-            
-            if(flag){
-                return temp;
-            }else{
-                return temp*-1;
+        if(a > b){
+            for(long i=b;i<=a;i++){
+                answer+=i;
             }
         }else{
-            long big = Math.max(a,b);
-            long small = Math.min(a,b);
-            long temp = 0;
-            long answer = 0;
-            
-            if(big+small == 0){
-                return 0;
-            }else if(Math.abs(big) > Math.abs(small)){
-                temp = Math.abs(small)+1;
-                for(long i=(int)temp;i<(int)big+1;i++){
-                    answer+=(long)i;
-                }
-            }else{
-                temp = Math.abs(big)+1;
-                for(long i=big;i<Math.abs(small)+1;i++){
-                    answer+=(long)i;
-                }
-                answer*=-1;
+            for(long i=a;i<=b;i++){
+                answer+=i;
             }
-            return answer;
         }
+        return answer;
+    }
+}
+
+------------------------------
+    
+// 등차수열의 합 공식을 이용한 모범답안
+class Solution {
+
+    public long solution(int a, int b) {
+        return sumAtoB(Math.min(a, b), Math.max(b, a));
+    }
+
+    private long sumAtoB(long a, long b) {
+        return (b - a + 1) * (a + b) / 2;
     }
 }
