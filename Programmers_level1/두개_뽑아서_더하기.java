cc96a58cae92a9e9438af194a7a2fef022935e03
Jinsan Kim <jinsan.dev@gmail.com>
2022-03-08 15:29:21 +0900

    Create 두개_뽑아서_더하기.java

---
+++ Programmers_level1/두개_뽑아서_더하기.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] numbers) {
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i=0;i<numbers.length-1;i++){
+            for(int j=i+1;j<numbers.length;j++){
+                if(list.contains(numbers[i]+numbers[j])){
+                    continue;
+                }else{
+                    list.add(numbers[i]+numbers[j]);
+                }
+            }
+        }
+        list.sort(Comparator.naturalOrder());
+        
+        int[] answer = new int[list.size()];
+        for(int i=0;i<list.size();i++){
+            answer[i] = list.get(i);
+        }
+        return answer;
+    }
+}
